0bde88f9fb9667088d8c0099b713ddffa449edc9
Prathyushkotla96 <33094244+Prathyushkotla96@users.noreply.github.com>
2020-01-20 18:49:13 +0530

    added authentication tokens using jwt, implemeted all the function such as create a user, create a task

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express=require('express')
+require('./db/mongoose')
+const User=require('./models/user')
+const Task=require('./models/tasks')
+const UserRouter=require('./routers/user')
+const TaskRouter=require('./routers/task')
+const app= express()
+const port=process.env.PORT || 3000
+
+app.use(express.json())
+app.use(UserRouter)
+app.use(TaskRouter)
+
+app.listen(port,()=>{
+    console.log('server is up on '+ port)
+})
