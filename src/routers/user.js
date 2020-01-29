4be8776bf44eb110fcab6e7ede89ec19e42c10de
Prathyushkotla96 <33094244+Prathyushkotla96@users.noreply.github.com>
2020-01-28 18:33:29 -0600

    file managment system is added to system, now users can upload their profile picture

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,8 @@ const express=require('express')
 const User=require('../models/user')
 const auth=require('../middleware/auth')
 const router=new express.Router()
+const multer=require('multer')
+const sharp=require('sharp')
 
 
 router.post('/users',async (req,res)=>{
@@ -18,6 +20,9 @@ router.post('/users',async (req,res)=>{
     }
 })
 
+
+
+
 router.get('/users/me',auth,async (req,res)=>{
     // try {
     //     const users=await User.find({})
@@ -28,6 +33,9 @@ router.get('/users/me',auth,async (req,res)=>{
     // }
     res.send(req.user)
 })
+
+
+
 
 router.patch('/users/me',auth, async (req, res) => {
     const updates = Object.keys(req.body)
@@ -54,6 +62,9 @@ router.patch('/users/me',auth, async (req, res) => {
     }
 })
 
+
+
+
 router.delete('/users/me', auth,async (req,res)=>{
     try {
         // const user=await User.findByIdAndDelete(req.user._id)
@@ -66,6 +77,9 @@ router.delete('/users/me', auth,async (req,res)=>{
         res.status(500).send(error)   
     }
 })
+
+
+
 
 router.post('/users/login',async (req,res)=>{
     try {
@@ -80,6 +94,9 @@ router.post('/users/login',async (req,res)=>{
 
 
 })
+
+
+
 
 router.post('/users/logout',auth,async(req,res)=>{
     try {
@@ -96,6 +113,9 @@ router.post('/users/logout',auth,async(req,res)=>{
     }
 })
 
+
+
+
 router.post('/users/logoutall',auth,async(req,res)=>{
     try {
         req.user.tokens=[]
@@ -106,6 +126,61 @@ router.post('/users/logoutall',auth,async(req,res)=>{
         
     }
 })
+
+const upload=multer({
+    limits:{
+        fileSize:1000000
+    },
+    fileFilter(req,file,cb){
+        if(!file.originalname.match(/\.(jpg|png|jpeg)$/)){
+            return cb(new Error('please upload a image'))
+        }
+        cb(undefined,true)
+    }
+
+})
+
+
+
+
+router.post('/users/me/avatar',auth,upload.single('avatar'),async(req,res)=>{
+    const buffer=await sharp(req.file.buffer).resize({width:250,height:250}).png().toBuffer()
+    req.user.avatar=buffer
+    await req.user.save() 
+    res.send()
+
+},(error,req,res,next)=>{
+    res.status(400).send({error:error.message});
+})
+
+router.delete('/users/me/avatar',auth,async(req,res)=>{
+    req.user.avatar=undefined
+    await req.user.save()
+    res.send()
+})
+
+
+
+
+router.get('/users/:id/avatar',async(req,res)=>{
+    try {
+        const user=await User.findById(req.params.id)
+
+        if(!user || !user.avatar){
+            throw new Error()
+
+        }
+
+        res.set('Content-Type','image/png')
+        res.send(user.avatar)
+        
+    } catch (error) {
+        res.status(404).send()
+    }
+})
+
+
+
 
 module.exports=router
 
